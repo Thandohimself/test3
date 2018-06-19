f69d70e65ec54dfc96d23bfe2c74f442fc5ff209
MxolisiHimself <37725776+MxolisiHimself@users.noreply.github.com>
2018-06-19 22:35:34 +0200

    Lungisani
    My first commit

---
+++ app/src/main/java/com/example/thando/testing3rep/LungisaniActivity.java
@@ -0,0 +1,13 @@
+package com.example.thando.testing3rep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LungisaniActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lungisani);
+    }
+}
